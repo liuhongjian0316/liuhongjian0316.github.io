8cc6d759b9c84d089b2590176f372d013dcf1733
aijiu <liuhongjian0316@outlook.com>
2022-04-23 21:28:35 +0800

    Site updated: 2022-04-23 21:28:31

--- js/rdmbkg.js
+++ js/rdmbkg.js
@@ -13,8 +13,9 @@ var bgindex =Math.floor(Math.random() * (backimg.length));
 //重设背景图片
 document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
 // 背景图片自适应
-document.getElementById("web_bg").style.backgroundSize = "100% 100%;";
+document.getElementById("web_bg").style.backgroundSize = "cover";
 document.getElementById("web_bg").style.backgroundRepeat = "no-repeat";
+document.getElementById("web_bg").style.backgroundPosition = "center center";
 //随机banner数组
 var bannerimg =[
     "url(/img/rbg1.jpg)",
